e7506edb21a1f2af95e6c612ecabe960b11179d9
grin <grinkiewicz@gmail.com>
2017-10-13 16:35:55 +0200

    [demo, modal, 4/5] Nasłuchuj zdarzeń nawigacyjnych w ekranie powitalnym

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,6 +3,11 @@ import { View, Text } from 'react-native';
 import styles from './styles';
 
 export default class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
   render() {
     return (
       <View style={styles.container}>
